fe50b992698e8b998d3e40e3b93aec2883fade9c
nilay <mailnilaydas@gmail.com>
2022-02-03 09:54:08 +0530

    implemented boxing/unboxing

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //primitive type
+        int a=100;
+        //boxing
+        //Integer x=new Integer(a);
+        //auto-boxing
+        Integer x=10;
+        //unboxing
+        //int result=x.intValue();
+        //auto-unboxing
+        int result=x;
     }
 }
