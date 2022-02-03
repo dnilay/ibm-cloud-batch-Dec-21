061c5c5f0a652868a736cf4f920b0cdfa7e712c7
nilay <mailnilaydas@gmail.com>
2022-02-03 10:17:32 +0530

    2nd implementation of boxing and unboxing

---
+++ src/com/example/Beer.java
@@ -0,0 +1,61 @@
+package com.example;
+
+public class Beer {
+    private String beerId;
+    private String beerName;
+    private Double costPerUnit;
+    private Integer unitInStock;
+
+    public Beer() {
+
+
+    }
+
+    public Beer(String beerId, String beerName, Double costPerUnit, Integer unitInStock) {
+        this.beerId = beerId;
+        this.beerName = beerName;
+        this.costPerUnit = costPerUnit;
+        this.unitInStock = unitInStock;
+    }
+
+    public String getBeerId() {
+        return beerId;
+    }
+
+    public void setBeerId(String beerId) {
+        this.beerId = beerId;
+    }
+
+    public String getBeerName() {
+        return beerName;
+    }
+
+    public void setBeerName(String beerName) {
+        this.beerName = beerName;
+    }
+
+    public Double getCostPerUnit() {
+        return costPerUnit;
+    }
+
+    public void setCostPerUnit(Double costPerUnit) {
+        this.costPerUnit = costPerUnit;
+    }
+
+    public Integer getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(Integer unitInStock) {
+        this.unitInStock = unitInStock;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "beerId=" + beerId  +
+                " beerName= " + beerName  +
+                " costPerUnit= " + costPerUnit +
+                " unitInStock= " + unitInStock ;
+    }
+}
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import java.util.Scanner;
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner=new Scanner( System.in);
 	// write your code here
         //primitive type
         int a=100;
@@ -14,5 +19,20 @@ public class Main {
         //int result=x.intValue();
         //auto-unboxing
         int result=x;
+
+        Beer myBeer=new Beer();
+        String[] str=UUID.randomUUID().toString().split("-");
+        myBeer.setBeerId(str[4]);
+        System.out.println("Enter Beer Name: ");
+        String name=scanner.next();
+        myBeer.setBeerName(name);
+        System.out.println("Enter Per Unit Cost: ");
+        double costPerUnit=scanner.nextDouble();
+        myBeer.setCostPerUnit(costPerUnit);
+        System.out.println("Enter Stock: ");
+        int stock=scanner.nextInt();
+        myBeer.setUnitInStock(stock);
+        System.out.println(myBeer);
+
     }
 }
