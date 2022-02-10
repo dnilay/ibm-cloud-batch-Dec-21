79b36c6bf08faf1e6765c5d4986295129d8c0ddf
nilay <mailnilaydas@gmail.com>
2022-02-10 11:39:19 +0530

    2nd  commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.NamedQuery;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -17,12 +23,26 @@ public class App
             SessionFactory sessionFactory=new Configuration().configure().addAnnotatedClass(Customer.class)
                     .buildSessionFactory();
             Session session=sessionFactory.openSession();
-            Customer customer=new Customer("John Doe");
+           /* Customer customer=new Customer("John Doe");
             session.getTransaction().begin();
             session.save(customer);
             session.getTransaction().commit();
-            System.out.println("customer saved... ");
+            System.out.println("customer saved... ");*/
+            session.getTransaction().begin();
+            TypedQuery query= session.createQuery("FROM Customer C WHERE C.customerName=:cName");
+            query.setParameter("cName","John1 Doe");
+            List<Customer> list=query.getResultList();
+            session.getTransaction().commit();
+            Iterator<Customer> iterator= list.iterator();
+            while (iterator.hasNext())
+            {
+                System.out.println(iterator.next());
+            }
 
+            session.getTransaction().begin();
+            Customer customer=session.find(Customer.class,3);
+            session.getTransaction().commit();
+            System.out.println(customer);
         }
         catch (Exception e)
         {
