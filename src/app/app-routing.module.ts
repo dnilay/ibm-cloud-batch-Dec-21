8ba9822063cac4868ab29ff561acdc2ce6a499bd
nilay <mailnilaydas@gmail.com>
2022-03-07 14:40:19 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {CreateEmployeeComponent} from "./create-employee/create-employee.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  {path: 'add',component: CreateEmployeeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
